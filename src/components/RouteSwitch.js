343ecd07e377b095d5cd13736baa605d77a91fca
pohjie <0xArlet@gmail.com>
2023-02-13 18:54:38 +0800

    Set up component skeleton for routing

---
+++ src/components/RouteSwitch.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+import Shop from './Shop';
+
+const RouteSwitch = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default RouteSwitch;
