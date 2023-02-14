c790f1fac2aec0160470fc63d198c027d6b3464b
pohjie <0xArlet@gmail.com>
2023-02-14 14:27:00 +0800

    Setup beautiful homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,14 +5,33 @@ import newBalanceImg from "../img/newbalance-bb550.webp";
 import reebokImg from "../img/reebok-classic.webp";
 import mizunoImg from "../img/mizuno-waverider10.webp";
 
+import { Link } from 'react-router-dom';
+
 const Home = () => {
+  const tagline = `Build your sneakers playground
+and your reputation renowned`;
+
   return (
     <div>
       <div className="sneakers-row">
-        <img className="sneaker-home" src={newBalanceImg} alt="New Balance BB550 Sneaker"></img>
-        <img className="sneaker-home" src={reebokImg} alt="Reebok Classic Sneaker"></img>
-        <img className="sneaker-home" src={mizunoImg} alt="Mizuno Waverider 10 Sneaker"></img>
+        <img
+          className="sneaker-home"
+          src={newBalanceImg}
+          alt="New Balance BB550 Sneaker"
+        ></img>
+        <img
+          className="sneaker-home"
+          src={reebokImg}
+          alt="Reebok Classic Sneaker"
+        ></img>
+        <img
+          className="sneaker-home"
+          src={mizunoImg}
+          alt="Mizuno Waverider 10 Sneaker"
+        ></img>
       </div>
+      <div className="home-stmt">{tagline}</div>
+      <div className="home-cta">Shop <u><Link to="/shop">now</Link></u></div>
     </div>
   );
 };
